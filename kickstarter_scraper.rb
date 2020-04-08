f6115b88e0f39d5e51b2676bd71b00e1ae43f72f
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-04-08 00:49:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,9 +11,9 @@ require "pry"
 # project.css("div.project-thumbnail a img").attribute("src").value
 # description:
 # project.css("p.bbcard_blurb").text
-# location: 
+# location:
 # project.css("ul.project-meta span.location-name").text
-# percent_funded: 
+# percent_funded:
 # project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 
